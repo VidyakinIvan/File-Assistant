a71399dfdea1f35ec77bfa5bcf7bfef66a3dd2e9
VidyakinIvan <D.V4an@yandex>
2024-03-05 01:24:16 +0300

    refactor cui and add view to implement controller and view of mvc architecture

---
+++ Controller.cs
@@ -0,0 +1,84 @@
+﻿namespace File_Assistant
+{
+    public class Controller
+    {
+        private List<Action<int>> Methods { get; }
+        private View View { get; }
+        readonly Tasks answers;
+        public Controller(Tasks answers)
+        {
+            this.answers = answers;
+            this.View = new();
+            Methods =
+            [
+                FileSystem,
+                FileStreams,
+                JSONSerialization,
+                XMLSerialization,
+                ZipArchives
+            ];
+            Run();
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string? command = View.GetCommand();
+                if (command == "0")
+                {
+                    break;
+                }
+                if (Int32.TryParse(command, out int result) && result > 0 && result <= 5 && answers.CanSolveTask(result))
+                {
+                    try
+                    {
+                        Methods[result - 1].Invoke(result - 1);
+                        View.ShowResult("Action executed successfully.");
+                    }
+                    catch (Exception ex)
+                    {
+                        View.ShowResult($"An error occurred: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    View.ShowResult("Invalid command. Please enter a number between 0 and 5.");
+                }
+            }
+        }
+        private void JSONSerialization(int num)
+        {
+            answers.FileName = View.GetFilename();
+            answers.InputPerson = View.PromptForPerson();
+            answers.SolveTask(num);
+        }
+        private void XMLSerialization(int num)
+        {
+            answers.FileName = View.GetFilename();
+            answers.InputPerson = View.PromptForPerson();
+            answers.SolveTask(num);
+        }
+        private void FileSystem(int num)
+        {
+            answers.SolveTask(num);
+        }
+        private void FileStreams(int num)
+        {
+            answers.FileName = View.GetFilename();
+            Console.WriteLine("Enter file content...");
+            answers.InputString = Console.ReadLine();
+            answers.SolveTask(num);
+        }
+        private void ZipArchives(int num)
+        {
+            Console.WriteLine("Enter achive name:");
+            answers.FileName = Console.ReadLine();
+            Console.WriteLine("Enter file path.");
+            answers.InputString = Console.ReadLine();
+            answers.SolveTask(num);
+        }
+    }
+
+}
+
---
+++ View.cs
@@ -0,0 +1,48 @@
+﻿namespace File_Assistant
+{
+    internal class View
+    {
+        public string? GetCommand() 
+        {
+            Console.WriteLine(@"Welcome to File Assistant console application!
+                        Please, enter the number of required action.
+                        1 - File system stats.
+                        2 - Text file actions.
+                        3 - JSON file actions.
+                        4 - XML file actions.
+                        5 - Zip archive actions.
+                        0 - Close application.");
+            string? command = Console.ReadLine();
+            return command;
+        }
+        public Person PromptForPerson()
+        {
+            Console.WriteLine("Enter your firstname.");
+            string? firstname = Console.ReadLine();
+            Console.WriteLine("Enter your lastname.");
+            string? lastname = Console.ReadLine();
+            Console.WriteLine("Enter your age.");
+            string? age = Console.ReadLine();
+            Console.WriteLine("Enter your country.");
+            string? country = Console.ReadLine();
+            Console.WriteLine("Enter your city.");
+            string? city = Console.ReadLine();
+            return new Person(
+                String.IsNullOrWhiteSpace(firstname) ? "The firstname is missing" : firstname,
+                String.IsNullOrWhiteSpace(lastname) ? "The lastname is missing" : lastname,
+                Int32.TryParse(age, out int result) ? result : 0,
+                String.IsNullOrWhiteSpace(country) ? "The country is missing" : country,
+                String.IsNullOrWhiteSpace(city) ? "The city is missing" : city);
+        }
+        public void ShowResult(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public string? GetFilename()
+        {
+            Console.WriteLine("Enter file name:");
+            return Console.ReadLine();
+        }
+    }
+}
